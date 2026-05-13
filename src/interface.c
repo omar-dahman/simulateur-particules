6a07de3e89ee92db53dbd42b97f80dfbc9918e29
Maxence Raymond <maxence.raymond@ensiie.eu>
2026-05-13 23:04:53 +0200

    Tâche E.5 : Ajout du rendu de la caméra

--- src/interface.c
+++ src/interface.c
@@ -1,28 +1,19 @@
-/**
- * \file    interface.c
- * \author  Omar DAHMAN
- * \date    2026-04-04
- *
- * \brief   User interface and I/O management (Implementation)
- *
- * \note MODIFIED FOR LOT E (Tâche E.2):
- *   - print_particule: now prints z-coordinate and vz.
- *   - print_environnement: now prints depth d.
- *   - is_pixel_occupied: uses 3D position but projects onto XY plane for PBM output.
- *   - snapshot / animate: unchanged in signature.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "interface.h"
 #include "environnement.h"
 #include "particules.h"
+#include "vector.h"
+#include "camera.h"
 
+#define FILENAME_BUFFER_LENGTH 256
 
 void print_particule(particule p) {
-    if (p == NULL) return;
+    if (p == NULL)
+        return;
 
     /* MODIFIED FOR LOT E (Tâche E.2): added z and vz */
     printf("Particule :\n");
@@ -31,7 +22,8 @@ void print_particule(particule p) {
 }
 
 void print_environnement(env e) {
-    if (e == NULL) return;
+    if (e == NULL)
+        return;
 
     /* MODIFIED FOR LOT E (Tâche E.2): added depth d */
     printf("Environnement :\n");
@@ -62,78 +54,88 @@ int get_int(void) {
         ret = scanf("%d", &value);
         if (ret != 1) {
             printf("Entrée invalide. Veuillez entrer un entier : ");
-            while (getchar() != '\n'); /* flush buffer */
+            while (getchar() != '\n'); // flush buffer
         }
     } while (ret != 1);
     return value;
 }
 
-float get_float(void) {
+float get_float() {
     float value;
     int ret;
     do {
         ret = scanf("%f", &value);
         if (ret != 1) {
             printf("Entrée invalide. Veuillez entrer un float : ");
-            while (getchar() != '\n'); /* flush buffer */
+            while (getchar() != '\n'); // nettoyage buffer
         }
     } while (ret != 1);
     return value;
 }
 
 
-/**
- * \brief   Test whether pixel (px, py) is occupied by any particle.
- * \details Projects 3D particle positions onto the XY plane to determine
- *          pixel occupancy for the PBM output.
- * \note MODIFIED FOR LOT E (Tâche E.2): get_x/get_y still used for XY
- *       projection; get_z is not needed for flat PBM (camera task handles 3D view).
- *
- * \param   e   environment
- * \param   px  pixel x-coordinate
- * \param   py  pixel y-coordinate
- * \param   wp  image width  in pixels
- * \param   hp  image height in pixels
- * \return  1 if occupied, 0 otherwise
- */
-static int is_pixel_occupied(env e, int px, int py, int wp, int hp) {
+// All below has been modified for tâche e.5
+
+short* render_2d(int hd, int wd, env e) {
+    short* render = calloc(hd * wd, sizeof (short));
     int n = get_n(e);
-    for (int i = 0; i < n; i++) {
+    float we = get_w(e);
+    float wh = get_h(e);
+    for(int i=0; i<n; i++) {
         particule p = get_particule(e, i);
-
-        /* Project 3D position onto XY plane */
-        int pixel_x = (int)(get_x(p) * wp / get_w(e));
-        int pixel_y = (int)(get_y(p) * hp / get_h(e));
-
-        if (pixel_x == px && pixel_y == py)
-            return 1;
+        int x = (int)(get_x(p) * wd / we);
+        int y = (int)(get_y(p) * hd / wh);
+        render[x*wd+y] = 1;
     }
-    return 0;
+    return render;
 }
 
-void snapshot(env e, char *path, int wp, int hp) {
-    FILE *file = fopen(path, "w");
-    if (file == NULL) return;
+short* render_3d(int hd, int wd, env e, Camera* cam) {
+    short* render = calloc(hd * wd, sizeof (short));
+    int n = get_n(e);
+    for(int i=0; i<n; i++) {
+        particule p = get_particule(e, i);
+        int px, py;
+        vec3 pos = vec3_make(get_x(p), get_y(p), get_z(p));
+        if (camera_is_particle_visible(cam, pos, &px, &py)==1) {
+            render[px*wd+py]=1;
+        }
+    }
+    return render;
+}
 
+void snapshot(env e, char *path, int wp, int hp, Camera* cam) {
+    FILE *file = fopen(path, "w");
+    short* render;
+    if (file == NULL)
+        return;
     fprintf(file, "P1\n");
-    fprintf(file, "# Snapshot environnement 3D (projection XY)\n");
+    fprintf(file, "# Snapshot environnement\n");
     fprintf(file, "%d %d\n", wp, hp);
 
-    for (int y = 0; y < hp; y++) {
-        for (int x = 0; x < wp; x++) {
-            fprintf(file, "%d ", is_pixel_occupied(e, x, y, wp, hp));
+    if (cam==NULL) {
+        render = render_2d(hp, wp, e);
+    } else {
+        render = render_3d(hp, wp, e, cam);
+    }
+
+    for (int x = 0; x < wp; x++) {
+        for (int y = 0; y < hp; y++) {
+            fprintf(file, "%d ", render[x*wp+y]);
         }
         fprintf(file, "\n");
     }
-
+    free(render);
     fclose(file);
 }
 
-void animate(env e, char *path, int wp, int hp, int t) {
-    char filename[256];
+void animate(env e, char *path, int wp, int hp, int t, Camera* cam) {
+    char filename[FILENAME_BUFFER_LENGTH];
+
     for (int i = 0; i < t; i++) {
-        snprintf(filename, sizeof(filename), "%s/env-%d.pbm", path, i);
-        snapshot(e, filename, wp, hp);
+        snprintf(filename, FILENAME_BUFFER_LENGTH, "%s/env-%d.pbm", path, i);
+        snapshot(e, filename, wp, hp, cam);
         move_particules(e);
+        camera_update(cam);
     }
 }
